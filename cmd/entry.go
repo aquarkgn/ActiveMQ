6dee5788d30b4c60d437264532438d3ee6838ded
GaoNan <alh9755@dingtalk.com>
2023-07-27 17:27:12 +0800

    ActiveMQ update

--- cmd/entry.go
+++ cmd/entry.go
@@ -1,54 +1,54 @@
 package main
 
 import (
-	"activemq/cmd/service"
 	"activemq/internal/app"
+	"fmt"
 	"github.com/go-stomp/stomp"
 	"log"
-	"os"
-	"os/signal"
-	"sync"
 	"time"
 )
 
 func main() {
-	// 创建连接
-	connOpts := []func(*stomp.Conn) error{
-		stomp.ConnOpt.Login(app.Username, app.Password),
-		stomp.ConnOpt.HeartBeat(12*time.Hour, 12*time.Hour), // 设置心跳检测间隔为10秒
+Loop:
+	for {
+		fmt.Println("启动ActiveMQ消费者...")
+		time.Sleep(5 * time.Second)
+
+		// Connect to broker
+		client, err := stomp.Dial("tcp", app.Url,
+			stomp.ConnOpt.Login(app.Username, app.Password),
+			stomp.ConnOpt.AcceptVersion(stomp.V12),
+			stomp.ConnOpt.HeartBeat(5*time.Second, 0*time.Second))
+		if err != nil {
+			fmt.Printf("连接到ActiveMQ失败, err: %v\n", err)
+			continue Loop
+		}
+		log.Printf("已连接到 ActiveMQ: %s", app.Url)
+		sub, err := client.Subscribe(app.QueueTest, stomp.AckClient, stomp.SubscribeOpt.Header("id", app.ConsumerName))
+		if err != nil {
+			fmt.Printf("订阅队列失败, err: %v\n", err)
+			continue Loop
+		}
+
+		fmt.Println("已启动ActiveMQ消费者, 正在等待消息...")
+		for {
+			msg := <-sub.C
+			if msg.Err != nil {
+				fmt.Printf("接收消息失败, err: %v\n", msg.Err)
+				err = client.Nack(msg)
+				if err != nil {
+					fmt.Printf("NACK失败, err: %v\n", err)
+					break
+				}
+			}
+
+			fmt.Println(string(msg.Body))
+			log.Printf("接收到消息：%s\n", string(msg.Body))
+			err = client.Ack(msg)
+			if err != nil {
+				fmt.Printf("ACK失败, err: %v\n", err)
+				break
+			}
+		}
 	}
-	conn, err := stomp.Dial(
-		"tcp",
-		app.BrokerAddr,
-		connOpts...,
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Disconnect()
-	log.Printf("已连接到 ActiveMQ: %s", app.BrokerAddr)
-
-	// 订阅队列
-	subQueue, err := service.ConsumerInstance.SubscribeQueue(conn, app.QueueName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("已订阅队列: %s", app.QueueName)
-
-	// 使用 WaitGroup 来等待消费者 goroutine 完成
-	var wg sync.WaitGroup
-
-	// 启动消费者
-	wg.Add(1)
-	service.ConsumerInstance.ConsumeQueueMessages(conn, subQueue, &wg)
-
-	// 等待中断信号，优雅地关闭连接
-	log.Printf("按 CTRL+C 退出")
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-	<-interrupt
-
-	// 等待消费者 goroutine 完成
-	wg.Wait()
-	log.Printf("已关闭通道")
 }
